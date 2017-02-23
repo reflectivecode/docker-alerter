b8dcd7154f454d9d326c2dd198ee8c9970623e19
Greg <reflectivecode@users.noreply.github.com>
2017-02-23 00:01:12 -0600

    add Dockerfile, script, and example

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # docker-alerter
 Send email alerts for docker events
+
+## Use
+
+```
+docker run \
+  -v /var/run/docker.sock:/var/run/docker.sock:ro \
+  -e "SSMTP_SERVER=email-smtp.us-east-1.amazonaws.com:465" \
+  -e "SSMTP_USER=SMTP_USERNAME" \
+  -e "SSMTP_PASS=SMTP_PASSWORD" \
+  -e "MAIL_TO=user@example.com" \
+  -e "MAIL_FROM=John Doe <user@example.com>" \
+  reflectivecode/docker-alerter
+```
---
+++ scripts/run-root.sh
@@ -0,0 +1,48 @@
+#!/bin/sh
+set -o errexit
+set -o pipefail
+set -o nounset
+
+echo "[i] create /etc/ssmtp/ssmtp.conf"
+{
+    echo "mailhub=${SSMTP_SERVER}";
+    echo "AuthUser=${SSMTP_USER}";
+    echo "AuthPass=${SSMTP_PASS}";
+    echo "FromLineOverride=yes";
+    echo "UseTLS=yes";
+} > /etc/ssmtp/ssmtp.conf
+
+unset SSMTP_SERVER SSMTP_USER SSMTP_PASS
+
+while true; do
+  curl --silent --show-error --unix-socket "${DOCKER_SOCKET}" -H "Accept: application/json" "http:/v1.24/events?filters=%7B%22type%22%3A%5B%22container%22%5D%7D" |
+    while read line; do
+        status=`echo "${line}" | jq .status -r`
+        if [[ "${status:0:5}" == "exec_" ]]; then
+            continue
+        fi
+        if [[ "${status}" == "attach" ]]; then
+            continue
+        fi
+        name=`echo "${line}" | jq .Actor.Attributes.name -r`
+        image=`echo "${line}" | jq .Actor.Attributes.image -r`
+        exitCode=`echo "${line}" | jq .Actor.Attributes.exitCode -r`
+        json=`echo "${line}" | jq '.'`
+        if [[ "${exitCode}" == "null" ]]; then
+            exitCode=""
+        else
+            exitCode="exitcode ${exitCode}"
+        fi
+            ssmtp "${MAIL_TO}" << EOF
+To: ${MAIL_TO}
+From: ${MAIL_FROM}
+Subject: [Docker Alert] ${name}
+
+Event: ${status} ${exitCode}
+Container: ${name}
+Image: ${image}
+
+${json}
+EOF
+    done
+done
